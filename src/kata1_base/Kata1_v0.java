6c503792fac7bf9c7fde2a8820028d5ae660cc9f
m2artigas <m2artigas@gmail.com>
2022-09-15 12:52:39 +0100

    version 3

--- src/kata1_base/Kata1_v0.java
+++ src/kata1_base/Kata1_v0.java
@@ -1,20 +1,13 @@
 package kata1_base;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
 
 
 public class Kata1_v0 {
 
     public static void main(String[] args) {
-	// borrar comentarios al final
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(2001, 1, 8);
-        //Person person = new Person ("koçak", new Date(101, 0, 8));
-        Person person = new Person ("koçak", date);
-        // a partir de 1900, 2011 101, meses entre 0 y 12, días iguales
+        LocalDate today = LocalDate.of(Integer.parseInt("2001"), Integer.parseInt("1"), Integer.parseInt("8"));
+        Person person = new Person ("koçak", today);
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
 }
