7fd0c67e5fb1a96a029464a63fc1e960d1af831a
m2artigas <m2artigas@gmail.com>
2022-09-15 12:32:59 +0100

    second commit

--- src/kata1_base/Kata1_v0.java
+++ src/kata1_base/Kata1_v0.java
@@ -1,12 +1,19 @@
 package kata1_base;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+
 
 public class Kata1_v0 {
 
     public static void main(String[] args) {
 	// borrar comentarios al final
-        Person person = new Person ("koçak", new Date(101, 0, 8));
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(2001, 1, 8);
+        //Person person = new Person ("koçak", new Date(101, 0, 8));
+        Person person = new Person ("koçak", date);
         // a partir de 1900, 2011 101, meses entre 0 y 12, días iguales
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
--- src/kata1_base/Person.java
+++ src/kata1_base/Person.java
@@ -1,12 +1,15 @@
 package kata1_base;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Person {
     private final String name;
-    private final Date birthdate;
+    private final Calendar birthdate;
+    private final long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
 
-    public Person(String name, Date birthdate) {
+    public Person(String name, Calendar birthdate) {
         this.name = name;
         this.birthdate = birthdate;
     }
@@ -17,11 +20,17 @@ public class Person {
         return this.name;
     }
 
-    public Date getBirthdate(){
+    public Calendar getBirthdate(){
         return birthdate;
     }
 
     public int getAge(){
-        return (int) ((new Date().getTime() - birthdate.getTime())/31536000000L);
+        Calendar today = GregorianCalendar.getInstance();
+        return (int) (milliSecondsToYear(today.getTimeInMillis() - birthdate.getTimeInMillis()));
+        //return (int) ((new Date().getTime() - birthdate.getTime())/31536000000L);
+    }
+    private long milliSecondsToYear(long milles){
+        return milles / MILLISECONDS_PER_YEAR;
+
     }
 }
