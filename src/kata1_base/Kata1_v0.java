ec3d6915460461d32641e235313c09ba6b49cd54
m2artigas <101264102+m2artigas@users.noreply.github.com>
2022-09-15 12:49:32 +0100

    sin comentarios

--- src/kata1_base/Kata1_v0.java
+++ src/kata1_base/Kata1_v0.java
@@ -8,13 +8,10 @@ import java.util.GregorianCalendar;
 public class Kata1_v0 {
 
     public static void main(String[] args) {
-	// borrar comentarios al final
         Calendar date;
         date = GregorianCalendar.getInstance();
         date.set(2001, 1, 8);
-        //Person person = new Person ("koçak", new Date(101, 0, 8));
         Person person = new Person ("koçak", date);
-        // a partir de 1900, 2011 101, meses entre 0 y 12, días iguales
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
 }
